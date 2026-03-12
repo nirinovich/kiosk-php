13ad98efefdf8ab9fac90bb5d747d18a758562df
nirinovich <nikitanirinovich@gmail.com>
2026-03-12 23:46:08 +0300

    traduce english to french

--- resources/js/components/user-menu-content.tsx
+++ resources/js/components/user-menu-content.tsx
@@ -53,7 +53,7 @@ export function UserMenuContent({ user }: Props) {
                         onClick={cleanup}
                     >
                         <Settings className="mr-2" />
-                        Settings
+                        Paramètres
                     </Link>
                 </DropdownMenuItem>
             </DropdownMenuGroup>
@@ -68,7 +68,7 @@ export function UserMenuContent({ user }: Props) {
                     data-test="logout-button"
                 >
                     <LogOut className="mr-2" />
-                    Log out
+                    Déconnexion
                 </Link>
             </DropdownMenuItem>
 
